5177fbdfeb1121dacd26bb693281ec6ab33064d4
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-04-15 16:15:23 -0500

    :memo: added logic to service worker to cache api calls for offline use

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,3 +70,23 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+// cache API calls for offline use
+self.onfetch = function(event) {
+  event.respondWith(
+       (async function() {
+          var cache = await caches.open(cacheName);
+          var cachedFiles = await cache.match(event.request);
+          if(cachedFiles) {
+              return cachedFiles;
+          } else {
+              try {
+                  var response = await fetch(event.request);
+                  await cache.put(event.request, response.clone());
+                  return response;
+              } catch(e) {
+                console.log(e)
+              }
+          }
+      }())
+  )
+}
